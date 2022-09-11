9489ec1a86f326e5e27c2be93810ae98a7406ea1
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-09-11 21:25:06 +0530

    Create Maximum_Performance_Of_A_Team.py

---
+++ Maximum_Performance_Of_A_Team.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxPerformance(self, n: int, speed: List[int], efficiency: List[int], k: int) -> int:
+        eng=[]
+        for eff,spd in zip(efficiency,speed) :
+            eng.append([eff,spd])
+        eng.sort(reverse=True)
+        res,speed=0,0
+        minHeap=[]
+        for eff,spd in eng :
+            if(len(minHeap)==k) :
+                speed -= heapq.heappop(minHeap)
+            speed +=spd
+            heapq.heappush(minHeap,spd)
+            res=max(res,eff*speed)
+        return res%(10**9 +7)
