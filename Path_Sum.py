2d529ca78b37790c40315db2aadde4cb1e9c6021
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-09-12 14:41:45 +0530

    Create Path_Sum.py

---
+++ Path_Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node,currSum) :
+            if not node :
+                return False
+            currSum +=node.val
+            if not node.left and not node.right :
+                return currSum==targetSum
+            return (dfs(node.left,currSum)or dfs(node.right,currSum))
+        return(dfs(root,0))
