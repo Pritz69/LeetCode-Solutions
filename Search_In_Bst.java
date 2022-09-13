a1b01fcaeb43ebb88ab732b7f0d4daa72213c526
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-09-13 14:49:43 +0530

    Create Search_In_Bst.java

---
+++ Search_In_Bst.java
@@ -0,0 +1,13 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root==null)
+            return null;
+        if(root.val==val)
+            return root;
+        else if(root.val>val)
+            return searchBST(root.left,val);
+        else 
+            return searchBST(root.right,val);
+        
+    }
+}
