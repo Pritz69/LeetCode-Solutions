4973dbbcaa613456630329a6e73e792e2eb8e7a7
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-10-04 01:26:57 +0530

    Create Keys_And_Rooms.java

---
+++ Keys_And_Rooms.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        HashSet<Integer>visited=new HashSet<>();
+        visited.add(0);
+        Stack<Integer> stack =new Stack<>();
+        stack.add(0);
+        while(!stack.isEmpty())
+        {
+            List<Integer>keys=rooms.get(stack.pop());
+            for (int key:keys)
+            {
+                if(!visited.contains(key))
+                {
+                    visited.add(key);
+                    stack.add(key);
+                }
+            }
+        }
+        return visited.size()==rooms.size();
+    }
+}
