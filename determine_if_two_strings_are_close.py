2490acdc6897c11ca8b96cd695fe482f91fa6d55
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-12-02 12:58:39 +0530

    Create determine_if_two_strings_are_close.py

---
+++ determine_if_two_strings_are_close.py
@@ -0,0 +1,7 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        c1=Counter(word1)
+        c2=Counter(word2)
+        n1=Counter([v for v in c1.values()])
+        n2=Counter([v for v in c2.values()])
+        return c1==c2 or (n1==n2 and set(word1)==set(word2))
