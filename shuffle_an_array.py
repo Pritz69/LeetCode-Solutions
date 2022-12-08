c26056467e96d6ea9076baa92a5553e9cf6cb568
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-12-08 13:46:49 +0530

    Create shuffle_an_array.py

---
+++ shuffle_an_array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def __init__(self, nums):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+        for i in range(len(self.array)):
+            swap_idx = random.randrange(i, len(self.array))
+            self.array[i], self.array[swap_idx] = self.array[swap_idx], self.array[i]
+        return self.array
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
