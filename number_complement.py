885fad5bc1bec4bbb70f01d876cc446417962f12
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-12-15 16:59:02 +0530

    Create number_complement.py

---
+++ number_complement.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findComplement(self, num: int) -> int:
+        s=list(bin(num)[2:])
+        for i in range(len(s)):
+            if s[i]=='0':
+                s[i]='1' 
+            elif s[i]=='1' :
+                s[i]='0'
+        return int(''.join(s),2)
