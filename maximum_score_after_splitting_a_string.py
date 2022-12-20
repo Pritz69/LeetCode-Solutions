32a1e047b0c1975a347b3afc8a5ac7a98c26c69c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-12-20 19:02:25 +0530

    Create maximum_score_after_splitting_a_string.py

---
+++ maximum_score_after_splitting_a_string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        m,n=0,len(s)
+        l=r=''
+        for i in range(1,n):
+            l,r=s[:i],s[i:]
+            c1=l.count('0')
+            c2=r.count('1')
+            m=max(m,c1+c2)
+        return m
+
+        
