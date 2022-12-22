c7501b7a99e3bb2363e922b26eb8f04f93860bcf
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-12-22 23:53:58 +0530

    Create valid_perfect_square.py

---
+++ valid_perfect_square.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPerfectSquare(self, num):
+        left = 0
+        right = num
+        
+        while left <= right:
+            mid = left + (right-left)//2
+            if  mid ** 2 == num:
+                return True
+            elif mid ** 2 > num:
+                right = mid -1
+            else:
+                left = mid +1
+        return False
