e7f05652a12d6bf9652e08f1a78bba431da61e6e
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2022-12-22 18:29:14 +0530

    Create kids_with_greatest_number_of_candies.py

---
+++ kids_with_greatest_number_of_candies.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        m=max(candies)
+        l=[]
+        for i in range(len(candies)):
+            if (candies[i]+extraCandies) >= m :
+                l.append(True)
+            else :
+                l.append(False)
+        return l
