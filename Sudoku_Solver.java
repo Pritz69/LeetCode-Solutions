a5f2874a48d2fd4052d083088ab9ceea3ad31a40
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-01-13 18:35:44 +0530

    Update Sudoku_Solver.java

--- Sudoku_Solver.java
+++ Sudoku_Solver.java
@@ -1,73 +1,36 @@
-import java.util.*;
 class Solution {
-   public boolean isSafe(char[][] board, int row, int col, int number) {
-       //column
-       for(int i=0; i<board.length; i++) {
-           if(board[i][col] == (char)(number+'0')) {
-               return false;
-           }
-       }
-      
-       //row
-       for(int j=0; j<board.length; j++) {
-           if(board[row][j] == (char)(number+'0')) {
-               return false;
-           }
-       }
-      
-       //grid
-       int sr = 3 * (row/3);
-       int sc = 3 * (col/3);
-      
-       for(int i=sr; i<sr+3; i++) {
-           for(int j=sc; j<sc+3; j++) {
-               if(board[i][j] == (char)(number+'0')) {
-                   return false;
-               }
-           }
-       }
-      
-       return true;
-   }
-  
-   public boolean helper(char[][] board, int row, int col) {
-       if(row == board.length) {
-           return true;
-       }
-      
-       int nrow = 0;
-       int ncol = 0;
-      
-       if(col == board.length-1) {
-           nrow = row + 1;
-           ncol = 0;
-       } else {
-           nrow = row;
-           ncol = col + 1;
-       }
-      
-       if(board[row][col] != '.') {
-           if(helper(board, nrow, ncol)) {
-               return true;
-           }
-       } else {
-          
-           //fill the place
-           for(int i=1; i<=9; i++) {
-               if(isSafe(board, row, col, i)) {
-                   board[row][col] = (char)(i+'0');
-                   if(helper(board, nrow, ncol))
-                       return true;
-                   else
-                        board[row][col] = '.';
-               }
-           }
-       }
-                     
-       return false;
-   }
-  
-   public void solveSudoku(char[][] board) {
-       helper(board, 0, 0);
-   }
+    public static boolean solveSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                        if (solveSudoku(board))
+                            return true;
+                        else
+                            board[i][j] = '.';
+                        }
+                    }
+                return false;
+                }
+            }
+        }
+        return true;
+  }
+  public static boolean isValid(char[][] board, int row, int col, char c) {
+    for (int i = 0; i < 9; i++) 
+    {
+      if (board[i][col] == c)
+        return false;
+
+      if (board[row][i] == c)
+        return false;
+
+      if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+        return false;
+    }
+    return true;
+  }
 }
