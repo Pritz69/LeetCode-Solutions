b58ff7fd249ec7f66789f2b5f0eaa4518111a3db
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-03-05 22:43:20 +0530

    Create jump_game4.java

---
+++ jump_game4.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        if(arr.length ==1 )
+            return 0;
+        int n = arr.length;
+        int step = 0;
+        HashMap<Integer,List <Integer>> map = new HashMap<>();
+        for(int i =0; i<n;i++)
+        {
+            map.computeIfAbsent(arr[i], v -> new ArrayList<Integer>()).add(i);
+        }
+        Queue <Integer> q = new LinkedList<>();
+        q.offer(0);
+        while(!q.isEmpty())
+        {
+            step++;
+            int size = q.size();
+            
+            for(int i = 0;i<size;i++)
+            {
+                int j = q.poll();
+                if(j-1 >=0 && map.containsKey(arr[j-1]))
+                {
+                    q.offer(j-1);
+                }
+                if(j+1<n && map.containsKey(arr[j+1]))
+                {
+                    if(j+1 == n-1)
+                        return step;
+                    q.offer(j+1);
+                }
+                if (map.containsKey(arr[j]))
+                {
+                    for(int k:map.get(arr[j]))
+                    {
+                        if(k!=j)
+                        {
+                            if (k==n-1)
+                                return step;
+                            q.offer(k);
+                        }
+                    }
+                    
+                }
+                map.remove(arr[j]);
+            }
+
+        }
+        return step;     
+    }
+}
+
+#Algorithms Made Easy
