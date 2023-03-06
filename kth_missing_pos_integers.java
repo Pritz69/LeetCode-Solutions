80551d31a40631c9b72346d4a3074be8345f9d1a
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-03-07 00:31:38 +0530

    Update kth_missing_pos_integers.java

--- kth_missing_pos_integers.java
+++ kth_missing_pos_integers.java
@@ -1,27 +1,23 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int i=1,j=0,ans=0;
-        while(k>0)
-        {
-            if(j>=arr.length)
-            {
-                ans=i;
-                i++;
-                k--;
-            }
-            else
-            if(arr[j]==i)
-            {
-                j++;
-                i++;
-            }
-            else
-            {
-                ans=i;
-                k--;
-                i++;
-            }
+        int n = arr.length;
+        int left = 0, right = n-1;
+        int missing  = compute(arr[n-1], n);
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            missing = compute(arr[mid], mid+1);
+            
+            if(missing >= k) right = mid-1;
+            else left = mid+1;
         }
-        return ans;
+        
+        // Right -> -1;
+        if(right == -1) return k;
+        return arr[right] + k-compute(arr[right], right+1);
+        
+    }
+    
+    int compute(int actual, int expected){
+        return actual - expected;
     }
 }
