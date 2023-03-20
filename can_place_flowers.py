854107fe53a8043681b34eed548f5353a1249ecd
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-03-20 18:51:41 +0530

    Create can_place_flowers.py

---
+++ can_place_flowers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        f=[0]+flowerbed+[0]
+        for i in range(1,len(f)-1) :
+            if f[i-1]==0 and f[i+1]==0 and f[i]==0 :
+                f[i]=1
+                n -=1
+        return n<=0
