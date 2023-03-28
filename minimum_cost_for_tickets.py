7bfa52953e05641b4b39d1805fa49ada73890775
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-03-28 20:15:25 +0530

    Create minimum_cost_for_tickets.py

---
+++ minimum_cost_for_tickets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        dp={}
+        for i in range(len(days)-1,-1,-1) :
+            dp[i]=float('inf')
+            for d,c in zip([1,7,30],costs) :
+                j=i
+                while j<len(days) and days[j]<days[i]+d :
+                    j +=1
+                dp[i]=min(dp[i],c + dp.get(j,0))
+        return dp[0]
