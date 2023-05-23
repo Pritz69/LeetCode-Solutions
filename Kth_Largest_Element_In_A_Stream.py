caed79999d9676855c0c82fb491054318ff77a8e
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-05-23 20:20:23 +0530

    Create Kth_Largest_Element_In_A_Stream.py

---
+++ Kth_Largest_Element_In_A_Stream.py
@@ -0,0 +1,18 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.minh,self.k=nums,k
+        heapq.heapify(self.minh)
+        while (len(self.minh) > self.k) :
+            heapq.heappop(self.minh)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.minh,val)
+        while (len(self.minh) > self.k) :
+            heapq.heappop(self.minh)
+        return self.minh[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
