4d730516cd5269e1c20806c222e86f230ea5e17c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-06-11 21:15:38 +0530

    Create Snapshot_Array.py

---
+++ Snapshot_Array.py
@@ -0,0 +1,24 @@
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.id = 0
+        self.history_records = [[[0, 0]] for _ in range(length)]
+        
+    def set(self, index: int, val: int) -> None:
+        self.history_records[index].append([self.id, val])
+
+    def snap(self) -> int:
+        self.id += 1
+        return self.id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        snap_index = bisect.bisect_right(self.history_records[index], [snap_id, 10 ** 9])
+        return self.history_records[index][snap_index - 1][1]
+        
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
