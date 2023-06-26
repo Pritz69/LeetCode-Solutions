7f3908a4e85bc92d2d1ca1eecccc238f98e9f85c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-06-26 23:33:44 +0530

    Create Contiguous_Subarray.py

---
+++ Contiguous_Subarray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        d={}
+        d[0]=-1
+        s=0
+        c=0
+        for i,v in enumerate(nums) :
+            if v==0 :
+                s -=1
+            else :
+                s +=1
+            if s in d :
+                c = max(c,i - d[s])
+            else :
+                d[s]=i
+        return c
+
