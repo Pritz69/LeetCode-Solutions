1ee077f66f565f01a61e8abb607d396cc3b14a18
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-08-07 00:21:52 +0530

    Create Faulty_KeyBoard.java

---
+++ Faulty_KeyBoard.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if (ch=='i')
+            {
+                sb.reverse();
+            }
+            else
+            {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
