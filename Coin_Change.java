227f5d26858857857699c0eee5a583b62d7b4c59
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-08-12 00:44:12 +0530

    Create Coin_Change.java

---
+++ Coin_Change.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int dp[]=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+        {
+            for(int j=0;j<coins.length;j++)
+            {
+                if (i-coins[j]>=0)
+                {
+                    dp[i]=Math.min(dp[i],1+dp[i-coins[j]]);
+                }
+            }
+        }
+        if (dp[amount]!=amount+1)
+        {
+            return dp[amount];
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
