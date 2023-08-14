3343bcc39c76e9f68a88a2960371bf9e95ea1aab
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-08-15 00:08:54 +0530

    Update Double_A_Number_Represented_As_A_LinkedList.java

--- Double_A_Number_Represented_As_A_LinkedList.java
+++ Double_A_Number_Represented_As_A_LinkedList.java
@@ -11,6 +11,47 @@
 class Solution {
     public ListNode doubleIt(ListNode l1) {
         Stack<Integer> s1=new Stack<Integer>();
+        ListNode res=null;
+        while (l1 != null)
+        {
+            s1.push(l1.val);
+            l1=l1.next;
+        }
+        int c=0;
+        while (!s1.isEmpty())
+        {     
+            int a=0; 
+            if (!s1.isEmpty())
+            {
+                a=s1.pop();
+            }
+            int t=a+a+c;
+            ListNode tmp=new ListNode(t%10);
+            c=t/10;
+            if (res==null)
+            {
+                res=tmp;
+            }
+            else
+            {
+                tmp.next=res;
+                res=tmp;
+            }
+        }
+        if(c!=0)
+        {
+            ListNode tmp=new ListNode(c);
+            tmp.next=res;
+            res=tmp;
+        }
+        return res;
+    }
+}
+// --- Below approach similar to add two numbers in LeetCode---
+/*
+class Solution {
+    public ListNode doubleIt(ListNode l1) {
+        Stack<Integer> s1=new Stack<Integer>();
         Stack<Integer> s2=new Stack<Integer>();
         ListNode res=null;
         ListNode curr=l1;
@@ -59,3 +100,4 @@ class Solution {
         return res;
     }
 }
+*/
