ede94a316be0ef305415e8f5735c80c1f69b681c
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-09-29 10:48:33 +0530

    Time: 874 ms (20.03%), Space: 31.2 MB (6.11%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.py
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -3,6 +3,24 @@ class Solution:
         fi=0
         fd=0
         for i in range(1,len(nums)) :
+            if nums[i]>=nums[i-1] :
+                continue
+            else :
+                fi=1
+        for i in range(1,len(nums)) :
+            if nums[i] <= nums[i-1] :
+                continue
+            else :
+                fd=1
+        if fi==0 or fd==0 :
+            return True
+        else :
+            return False
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        fi=0
+        fd=0
+        for i in range(1,len(nums)) :
             if nums[i] > nums[i-1] :
                 fi=1
             if nums[i] < nums[i-1] :
