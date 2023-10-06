793a6de09d16a62c8cb3449d660fb6b19814e489
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-10-06 12:46:44 +0530

    Update 0343-integer-break.py

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,3 +1,16 @@
+# Linear
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n==2 or n==3 :
+            return n-1
+        if n==4 :
+            return 4
+        res=1
+        while n > 4 :
+            n -=3
+            res *= 3
+        return n*res
+# Top Down
 class Solution:
     def integerBreak(self, n: int) -> int:
         dp={1:1}
@@ -11,3 +24,14 @@ class Solution:
             return dp[n]
 
         return rec(n)
+
+# Bottom Up
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp={1:1}
+        for num in range(2,n+1):
+            dp[num]=0 if num==n else num
+            for i in range(1,num):
+                val=dp[i]*dp[num-i]
+                dp[num]=max(dp[num],val)
+        return dp[n]
