d808cf34f1a50d98a48c5529558c070ce0ea0793
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-10-15 21:12:49 +0530

    Time: 603 ms (15.55%), Space: 106.5 MB (32.22%) - LeetHub

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/1398-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numWays(self, steps: int, arrLen: int) -> int:
+        dp={}
+        mod=10**9 + 7
+        
+        def rec(ind,steps) :
+            if ind==arrLen or ind<0:
+               return 0
+            if steps==0 :
+                if ind==0 :
+                    return 1
+                else :
+                    return 0
+            if (ind,steps) in dp :
+                return dp[(ind,steps)] 
+            mover,stay,movel=0,0,0
+            mover += rec(ind+1,steps-1)
+            movel +=rec(ind-1,steps-1)
+            stay += rec(ind,steps-1)
+
+            dp[(ind,steps)] = (mover + stay + movel)%mod
+            return dp[(ind,steps)]
+
+        return rec(0,steps)
