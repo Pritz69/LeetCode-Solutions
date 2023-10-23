6f3551af722f75e28369be43c909b3eedbe5f775
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-10-23 12:22:43 +0530

    Time: 47 ms (13.85%), Space: 16.3 MB (8.9%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,3 +1,5 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         return n&(n-1)==0 and (n-1)%3 == 0
+
+        # n&(n-1) filters out all the powers of 2, now not all powers of 2 are powers of 4, but the alternate ones, more precisely the one which have multiple of 3 before it, example 16,64 and not 8 and 32.   
