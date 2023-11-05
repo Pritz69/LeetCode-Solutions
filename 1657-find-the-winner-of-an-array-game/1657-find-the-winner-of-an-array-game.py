a7dcc25ba7bd7a4a6b7a4835adf7362087b6d423
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-11-05 21:54:50 +0530

    Time: 524 ms (71.2%), Space: 30.1 MB (35.87%) - LeetHub

--- 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
+++ 1657-find-the-winner-of-an-array-game/1657-find-the-winner-of-an-array-game.py
@@ -1,17 +1,16 @@
 class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
-        st=1
-        ma=max(arr[0],arr[1])
-        if st==k :
-            return ma
-        for i in range(2,len(arr)) :
-            if arr[i] > ma :
-                st=1 
-                ma = arr[i]
-                if st==k :
-                    return ma
-            else :
-                st +=1
-                if st==k :
-                    return ma
-        return ma
+        curr_winner = arr[0]
+        win_count = 0
+
+        for i in range(1, len(arr)):
+            if curr_winner > arr[i]:
+                win_count += 1
+            else:
+                curr_winner = arr[i]
+                win_count = 1
+
+            if win_count == k:
+                return curr_winner
+        
+        return curr_winner
