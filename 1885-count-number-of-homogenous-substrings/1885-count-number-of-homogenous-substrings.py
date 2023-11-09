328f43337ddd797e1790187317238c5561522b32
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-11-09 12:12:36 +0530

    Time: 185 ms (13.67%), Space: 17.4 MB (52.88%) - LeetHub

---
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countHomogenous(self, s: str) -> int:
+        d={}
+        for x in s :
+           d[x]=d.get(x,0)+1
+        tc=0
+        cs=1
+        for i in range(1,len(s)) :
+            if s[i]==s[i-1] :
+                cs +=1
+            else :
+                tc += (cs*(cs-1))//2
+                cs =1
+        tc += (cs*(cs-1))//2
+        for x in d :
+            tc += d[x]
+        mod=10**9 + 7
+        return tc%(mod)
