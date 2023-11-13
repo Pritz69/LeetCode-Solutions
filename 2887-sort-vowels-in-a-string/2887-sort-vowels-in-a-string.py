ab2fca869aa70256eabcaf71618953a94c659e79
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-11-13 16:17:52 +0530

    Time: 140 ms (69.44%), Space: 19.7 MB (37.14%) - LeetHub

--- 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/2887-sort-vowels-in-a-string.py
@@ -1,16 +1,20 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
         v=[]
+        c=[]
         for x in s :
-            if x=='A' or x=='a' or x=='E' or x=='e' or x=='I' or x=='i' or x=='O' or x=='o' or x=='U' or x=='u' :
+            if x in "AEIOUaeiou" :
                 v.append(x)
+            else :
+                c.append(x)
         v.sort()
-        i=0
         ans=[]
-        for x in s :
-            if x=='A' or x=='a' or x=='E' or x=='e' or x=='I' or x=='i' or x=='O' or x=='o' or x=='U' or x=='u' :
+        i,j=0,0
+        for k,x in enumerate(s) :
+            if x in "AEIOUaeiou" :
                 ans.append(v[i])
                 i +=1
             else :
-                ans.append(x)
+                ans.append(c[j])
+                j +=1
         return ''.join(ans)
