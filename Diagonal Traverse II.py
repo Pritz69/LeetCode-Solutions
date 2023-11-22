c38aba2ab1939ef87c3fdf1ab8cb0cd52404c7cf
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-11-22 13:54:08 +0530

    Create Diagonal Traverse II.py

---
+++ Diagonal Traverse II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
+        d=defaultdict(list)
+        for i in range(len(nums)-1,-1,-1) :
+            for j in range(len(nums[i])-1,-1,-1) :
+                d[i+j].append(nums[i][j])
+        ans=[]
+        nd=dict(sorted(d.items(), key = lambda x: x[0]))
+        #print(nd)
+        for x in nd :
+            l=d[x]
+            #l.sort(reverse=True)
+            ans = ans + l
+        return ans
