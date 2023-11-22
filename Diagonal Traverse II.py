3d2ad4fdf01b9da8c453d5b0a84d490452d3ab15
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2023-11-22 21:24:48 +0530

    Update Diagonal Traverse II.py

--- Diagonal Traverse II.py
+++ Diagonal Traverse II.py
@@ -5,10 +5,15 @@ class Solution:
             for j in range(len(nums[i])-1,-1,-1) :
                 d[i+j].append(nums[i][j])
         ans=[]
-        nd=dict(sorted(d.items(), key = lambda x: x[0]))
+        #nd=dict(sorted(d.items(), key = lambda x: x[0]))
         #print(nd)
-        for x in nd :
-            l=d[x]
-            #l.sort(reverse=True)
-            ans = ans + l
+        #for x in nd :
+        #    l=d[x]
+        #    #l.sort(reverse=True)
+        #    ans = ans + l
+        m=0
+        for x in d :
+            m=max(m,x)
+        for i in range(m+1) :
+            ans +=d[i]
         return ans
