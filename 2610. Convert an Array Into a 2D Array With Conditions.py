c77271a3a7e17e1b3d47d6f975462af8b5101b21
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-01-02 11:47:12 +0530

    Update 2610. Convert an Array Into a 2D Array With Conditions.py

--- 2610. Convert an Array Into a 2D Array With Conditions.py
+++ 2610. Convert an Array Into a 2D Array With Conditions.py
@@ -14,3 +14,21 @@ class Solution:
                     c +=1
             l.append(nl)
         return l
+
+
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        
+        freq = [0] * (len(nums) + 1)
+        ans = []
+
+        for c in nums:
+            #frequency of the integer c is greater than or equal to the number of rows in the matrix
+            if freq[c] >= len(ans):
+                ans.append([])
+
+            # Store the integer in the list corresponding to its current frequency.
+            ans[freq[c]].append(c)
+            freq[c] += 1
+
+        return ans
