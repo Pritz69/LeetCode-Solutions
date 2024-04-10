37f930dac063c7ea6bc3abfd078b54a60125dd4b
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-04-10 12:07:47 +0530

    Create 950. Reveal Cards In Increasing Order.py

---
+++ 950. Reveal Cards In Increasing Order.py
@@ -0,0 +1,11 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        deck.sort()
+        res=[0]*len(deck)
+        q=deque(range(len(deck)))
+        for n in deck :
+            i=q.popleft()
+            res[i]=n
+            if q :
+                q.append(q.popleft())
+        return res
