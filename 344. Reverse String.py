3d4981a2d421a1d79f6d3d39bb689ab5e4d2c4f4
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-06-02 16:05:17 +0530

    Create 344. Reverse String.py

---
+++ 344. Reverse String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l,r=0,len(s)-1
+        while l <= r:
+            t=s[l]
+            s[l]=s[r]
+            s[r]=t
+            l +=1
+            r -=1
