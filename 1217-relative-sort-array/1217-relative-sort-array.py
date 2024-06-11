43b9774096b7d2cbf9742501eee9c829c42c8df6
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-06-11 12:42:12 +0530

    Time: 41 ms (57.03%), Space: 16.6 MB (67.06%) - LeetHub

---
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        d={}
+        for x in arr1:
+            d[x]=d.get(x,0)+1
+        i =0
+        for x in arr2 :
+            for _ in range(d[x]) :
+                arr1[i]=x
+                i +=1
+            d.pop(x)
+        d=dict(sorted(d.items(),key=lambda x:x[0]))
+        for x in d :
+            for _ in range(d[x]) :
+                arr1[i]=x
+                i +=1
+        return arr1
+
+
+        
