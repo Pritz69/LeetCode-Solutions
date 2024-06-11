c36034fb7ad2dc4d787e8f8d6ab1364140913b6d
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-06-11 12:43:30 +0530

    Time: 41 ms (57.03%), Space: 16.7 MB (67.06%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -9,10 +9,10 @@ class Solution:
                 arr1[i]=x
                 i +=1
             d.pop(x)
-        d=dict(sorted(d.items(),key=lambda x:x[0]))
+        d=sorted(d.items(),key=lambda x:x[0])
         for x in d :
-            for _ in range(d[x]) :
-                arr1[i]=x
+            for _ in range(x[1]) :
+                arr1[i]=x[0]
                 i +=1
         return arr1
 
