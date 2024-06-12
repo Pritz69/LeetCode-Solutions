66763323e0efa2223e4e3631c144f0376efe00f4
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-06-12 11:43:59 +0530

    Time: 37 ms (57.63%), Space: 16.5 MB (79.02%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,22 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        a,b,c=0,0,0
-        for x in nums :
-            if x==0 :
-                a +=1
-            elif x==1 :
-                b +=1
-            else :
-                c +=1
-        for i in range(len(nums)) :
-            if a > 0 :
-                nums[i]=0
-                a -=1
-            elif b > 0 :
-                nums[i]=1
-                b-=1
-            else :
-                nums[i]=2
-                c -=1
-        print(nums)
+        red, white, blue = 0, 0, len(nums)-1
+    
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
