4f48315121ad6c5931c80e1d7d7da088c6566ef3
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-06-22 18:38:37 +0530

    Time: 651 ms (27.32%), Space: 23.6 MB (42.15%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -2,10 +2,11 @@ class Solution:
     def numberOfSubarrays(self, A, k):
         def atMost(k):
             res = i = 0
+            c=0
             for j in range(len(A)):
-                k -= A[j] % 2
-                while k < 0:
-                    k += A[i] % 2
+                c += A[j] % 2
+                while c > k:
+                    c -= A[i] % 2
                     i += 1
                 res += j - i + 1
             return res
