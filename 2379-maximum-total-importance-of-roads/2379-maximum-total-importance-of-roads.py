2c80989789c87557025edce877bc52d814a2142f
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-06-28 11:51:52 +0530

    Time: 1248 ms (56.15%), Space: 42.5 MB (49.18%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
@@ -5,13 +5,8 @@ class Solution:
             d[x[0]]=d.get(x[0],0) +1
             d[x[1]]=d.get(x[1],0) +1
         d=sorted(d.items(),key=lambda x:(x[1]),reverse=True)
-        l=[]
-        i=n
-        for x in d :
-            l.append((x[1],i))
-            i -=1
-        #print(l)
         ans=0
-        for x in l :
-            ans += x[0]*x[1]
+        for x in d :
+            ans += x[1] * n
+            n -=1
         return ans 
