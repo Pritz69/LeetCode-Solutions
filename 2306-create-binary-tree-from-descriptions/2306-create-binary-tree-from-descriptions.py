ef52385c78e43e94c9ad03165aab57595b8b2368
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-15 10:48:18 +0530

    Time: 1577 ms (52.53%), Space: 25.5 MB (96.3%) - LeetHub

--- 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.py
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.py
@@ -12,23 +12,19 @@ class Solution:
         for x in descriptions :
             d[x[0]].append([x[1],x[2]])
             s.add(x[1])
-        r=0
         for x in d.keys() :
             if x not in s :
-                r=x
+                root=TreeNode(x)
                 break
-        root=TreeNode(r)
         q=deque()
         q.append(root)
         while q :
             node=q.popleft()
             for x in d[node.val] :
-                if x[1]==1 :
+                if x[1]:
                     node.left=TreeNode(x[0])
                     q.append(node.left)
                 else :
                     node.right=TreeNode(x[0])
                     q.append(node.right)
-        #print(d)
-        #print(s)
         return root
