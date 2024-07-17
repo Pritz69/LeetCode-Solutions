d7cab1d5f682d8512db77adeb5678101daca26dd
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-17 11:06:41 +0530

    Time: 54 ms (63.67%), Space: 16.8 MB (93.78%) - LeetHub

--- 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
@@ -6,16 +6,20 @@
 #         self.right = right
 class Solution:
     def delNodes(self, root, to_delete):
-        to_delete_set = set(to_delete)
+        to_delete = set(to_delete)
         res = []
-
-        def helper(root, is_root):
-            if not root: return None
-            root_deleted = root.val in to_delete_set
-            if is_root and not root_deleted:
-                res.append(root)
-            root.left = helper(root.left, root_deleted)
-            root.right = helper(root.right, root_deleted)
-            return None if root_deleted else root
-        helper(root, True)
+        def walk(root, parent_exist):
+            if root is None:
+                return None
+            if root.val in to_delete:
+                root.left = walk(root.left, parent_exist=False)
+                root.right = walk(root.right, parent_exist=False)
+                return None
+            else:
+                if not parent_exist:
+                    res.append(root)
+                root.left = walk(root.left, parent_exist=True)
+                root.right = walk(root.right, parent_exist=True)
+                return root
+        walk(root, parent_exist=False)
         return res
