db5a531417e56c8e7967ecaad2e934229d7ecadc
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-17 11:03:32 +0530

    Time: 56 ms (51.04%), Space: 17 MB (24.3%) - LeetHub

---
+++ 1207-delete-nodes-and-return-forest/1207-delete-nodes-and-return-forest.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root, to_delete):
+        to_delete_set = set(to_delete)
+        res = []
+
+        def helper(root, is_root):
+            if not root: return None
+            root_deleted = root.val in to_delete_set
+            if is_root and not root_deleted:
+                res.append(root)
+            root.left = helper(root.left, root_deleted)
+            root.right = helper(root.right, root_deleted)
+            return None if root_deleted else root
+        helper(root, True)
+        return res
