67145c73ee4c10f478e2f0726479ec89edeceeaa
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-07-24 10:53:37 +0530

    Time: 953 ms (78.8%), Space: 24.8 MB (47.83%) - LeetHub

--- 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
+++ 1333-sort-the-jumbled-numbers/1333-sort-the-jumbled-numbers.py
@@ -4,14 +4,13 @@ class Solution:
         for x in nums :
             nn=""
             t=x
-            if t==0 :
-                ans.append((x,mapping[0]))
-                continue
             while t :
                 d=t%10
                 nn = str(mapping[d]) + nn
                 t=t//10
             if nn :
                 ans.append((x,int(nn)))
+            else :
+                ans.append((x,mapping[0]))
         ans=sorted(ans, key=lambda x:x[1])
         return [x[0] for x in ans]
