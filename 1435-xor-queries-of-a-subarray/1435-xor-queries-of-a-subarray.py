04b42cdae2c95c89d0b3d06c86c591c4b0b37809
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-09-13 10:27:16 +0530

    Time: 304 ms (54.49%), Space: 31.5 MB (10.11%) - LeetHub

---
+++ 1435-xor-queries-of-a-subarray/1435-xor-queries-of-a-subarray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        ans=[]
+        xol=[]
+        x=0
+        for e in arr :
+            x=x^e
+            xol.append(x)
+        xor=[0]*len(arr)
+        xr=0
+        for i in range(len(arr)-1,-1,-1) :
+            xr=xr^arr[i]
+            xor[i]=xr
+        print(xol)
+        print(xor)
+        for q in queries :
+            l,r=q[0],q[1]
+            a=x
+            if l-1 >= 0 :
+                a=a^xol[l-1]
+            if r+1 < len(arr) :
+                a=a^xor[r+1]
+            ans.append(a)
+        return ans
+        
