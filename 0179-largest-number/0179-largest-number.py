849a6afcb6241b2071d246d28c44e10cfc3fb27f
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-09-18 10:14:13 +0530

    Time: 38 ms (74.95%), Space: 16.6 MB (32.99%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        def cmp_func(x, y):
+            """Sorted by value of concatenated string increasingly."""
+            if x + y > y + x:
+                return 1
+            elif x == y:
+                return 0
+            else:
+                return -1
+            
+        # Build nums contains all numbers in the String format.
+        nums = [str(num) for num in nums]
+        
+        # Sort nums by cmp_func decreasingly.
+        nums.sort(key = cmp_to_key(cmp_func), reverse = True)
+        
+        # Remove leading 0s, if empty return '0'.
+        return ''.join(nums).lstrip('0') or '0'
