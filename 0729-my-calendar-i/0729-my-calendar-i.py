10f0d1b22441250f130c7dbe225064065fc01a45
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-09-26 12:08:44 +0530

    Time: 1362 ms (5%), Space: 17.2 MB (89.44%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -2,12 +2,20 @@ class MyCalendar:
 
     def __init__(self):
         self.bookings = []
+        self.max_end = 0  
 
     def book(self, start: int, end: int) -> bool:
+        if start >= self.max_end:
+            self.bookings.append((start, end))
+            self.max_end = max(self.max_end, end)
+            return True
+        
         for s, e in self.bookings:
-            if max(s, start) < min(e, end): 
+            if max(s, start) < min(e, end):  
                 return False
+        
         self.bookings.append((start, end))
+        self.max_end = max(self.max_end, end)
         return True
 
 
