bc4b7e1cc83bd787d026efff2a38e95aee7f4715
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-10-07 09:09:45 +0530

    Time: 43 ms (63.91%), Space: 16.6 MB (41.77%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -1,13 +1,10 @@
 class Solution:
     def minLength(self, s: str) -> int:
-        st = s[:2]  
-        for i in range(2, len(s)):
-            while len(st) >= 2 and (st[-2] + st[-1] == "AB" or st[-2] + st[-1] == "CD"):
-                st = st[:-2]  
-            
-            st += s[i]
-        
-        while len(st) >= 2 and (st[-2] + st[-1] == "AB" or st[-2] + st[-1] == "CD"):
-            st = st[:-2]
-        
+        st = []
+        for ch in s:
+            # Check the last two characters in the stack to remove "AB" or "CD"
+            if st and ((st[-1] == 'A' and ch == 'B') or (st[-1] == 'C' and ch == 'D')):
+                st.pop()  # Remove the last character from the stack
+            else:
+                st.append(ch)  # Append the current character to the stack
         return len(st)
