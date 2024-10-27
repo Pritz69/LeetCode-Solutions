dced5a338d9e3056a8aec0c988eab94c57303a87
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-10-27 21:17:21 +0530

    Time: 90 ms (34.78%), Space: 19.3 MB (20.45%) - LeetHub

---
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countSquares(self, A):
+        for i in range(1, len(A)):
+            for j in range(1, len(A[0])):
+                A[i][j] *= min(A[i - 1][j], A[i][j - 1], A[i - 1][j - 1]) + 1
+        return sum(map(sum, A))
