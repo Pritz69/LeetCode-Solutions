20f64215aa2a6f68796c748e18f96e70010f3338
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-11-24 22:35:02 +0530

    Time: 105 ms (31.36%), Space: 25 MB (72.1%) - LeetHub

--- 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
+++ 2089-maximum-matrix-sum/2089-maximum-matrix-sum.py
@@ -7,8 +7,9 @@ class Solution:
             for j in range(len(matrix[0])) :
                 if (matrix[i][j] < 0) :
                     c=c+1
-                s=s+abs(matrix[i][j])    
-                m=min(m,abs(matrix[i][j]))
+                e=abs(matrix[i][j])
+                s=s+e    
+                m=min(m,e)
         if c%2==1 :
             return s-(m*2)
         else :
