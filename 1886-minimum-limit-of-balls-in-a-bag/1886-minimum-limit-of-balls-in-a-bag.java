b1aa35ac7892fcfd4e28190378e7288785b85bd7
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-12-07 23:21:35 +0530

    Time: 26 ms (93.1%), Space: 59.8 MB (84.82%) - LeetHub

---
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -0,0 +1,15 @@
+class Solution {
+        public int minimumSize(int[] A, int k) {
+        int left = 1, right = 1_000_000_000;
+        while (left < right) {
+            int mid = (left + right) / 2, count = 0;
+            for (int a : A)
+                count += (a - 1) / mid;
+            if (count > k)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
