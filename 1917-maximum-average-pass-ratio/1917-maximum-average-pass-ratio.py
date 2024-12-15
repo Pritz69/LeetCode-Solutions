2608a797a42d50e14f892075ca93086b549c71c5
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-12-15 19:38:36 +0530

    Time: 1203 ms (34.66%), Space: 54.6 MB (23.46%) - LeetHub

--- 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.py
@@ -5,9 +5,8 @@ class Solution:
 
         maxHeap = []
         for a, b in classes:
-            a, b = a * 1.0, b * 1.0  # Convert int to double
             maxHeap.append((-profit(a, b), a, b))
-        heapq.heapify(maxHeap)  # Heapify maxHeap cost O(N)
+        heapq.heapify(maxHeap) 
 
         for _ in range(extraStudents):
             d, a, b = heapq.heappop(maxHeap)
