e381be8b18d50bec97e9944db18f114287dc33f8
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-12-17 13:24:45 +0530

    Time: 227 ms (57.61%), Space: 19.3 MB (22.73%) - LeetHub

--- 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/2300-construct-string-with-repeat-limit.py
@@ -1,24 +1,19 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
         freq = Counter(s)
-        sorted_chars = sorted(freq.keys(), reverse=True)  # Descending order of characters
+        sorted_chars = sorted(freq.keys(), reverse=True)  
         ans = []
-
         while sorted_chars:
-            # Pick the highest available character
             curr = sorted_chars[0]
             if freq[curr] > 0:
-                # Add up to repeatLimit of the current character
                 count = min(freq[curr], repeatLimit)
                 ans.append(curr * count)
                 freq[curr] -= count
 
                 if freq[curr] == 0:
-                    # Remove the character from sorted_chars if exhausted
                     sorted_chars.pop(0)
 
                 if freq[curr] > 0:
-                    # If the character still exists, add the next available character
                     if len(sorted_chars) > 1:
                         next_char = sorted_chars[1]
                         ans.append(next_char)
@@ -30,7 +25,6 @@ class Solution:
                         # If no next character, stop to avoid consecutive repeats
                         break
             else:
-                # Remove exhausted character
                 sorted_chars.pop(0)
 
         return ''.join(ans)
