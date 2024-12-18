0aa1d892ed720d29e6d13d3e12af8c2af2669b6a
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-12-18 12:29:05 +0530

    Time: 4 ms (21.42%), Space: 17.6 MB (10.22%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,14 +1,10 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        d={}
-        for i,x in enumerate(prices) :
-            d[i]=d.get(i,0)+x
         for i in range(len(prices)) :
             v=0
-            ind=i+1
-            while ind in d and d[ind] > prices[i] :
-                ind +=1
-            if ind in d :
-                v=d[ind]
-            prices[i]=prices[i]-v
+            for j in range(i+1,len(prices)) :
+                if prices[j] <= prices[i] :
+                    v=prices[j]
+                    break
+            prices[i] -= v
         return prices
