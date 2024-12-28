c02a7cf6e61012e17ee48cdf644fae0c79ef5451
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-12-28 18:16:15 +0530

    Time: 19 ms (89.44%), Space: 19.5 MB (77.78%) - LeetHub

---
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        # Best single, double, and triple sequence found so far
+        bestSeq = 0
+        bestTwoSeq = [0, k]
+        bestThreeSeq = [0, k, k*2]
+
+        # Sums of each window
+        seqSum = sum(nums[0:k])
+        seqTwoSum = sum(nums[k:k*2])
+        seqThreeSum = sum(nums[k*2:k*3])
+
+        # Sums of combined best windows
+        bestSeqSum = seqSum
+        bestTwoSum = seqSum + seqTwoSum
+        bestThreeSum = seqSum + seqTwoSum + seqThreeSum
+
+        # Current window positions
+        seqIndex = 1
+        twoSeqIndex = k + 1
+        threeSeqIndex = k*2 + 1
+        while threeSeqIndex <= len(nums) - k:
+            # Update the three sliding windows
+            seqSum = seqSum - nums[seqIndex - 1] + nums[seqIndex + k - 1]
+            seqTwoSum = seqTwoSum - nums[twoSeqIndex - 1] + nums[twoSeqIndex + k - 1]
+            seqThreeSum = seqThreeSum - nums[threeSeqIndex - 1] + nums[threeSeqIndex + k - 1]
+            
+            # Update best single window
+            if seqSum > bestSeqSum:
+                bestSeq = seqIndex
+                bestSeqSum = seqSum
+
+            # Update best two windows
+            if seqTwoSum + bestSeqSum > bestTwoSum:
+                bestTwoSeq = [bestSeq, twoSeqIndex]
+                bestTwoSum = seqTwoSum + bestSeqSum
+
+            # Update best three windows
+            if seqThreeSum + bestTwoSum > bestThreeSum:
+                bestThreeSeq = bestTwoSeq + [threeSeqIndex]
+                bestThreeSum = seqThreeSum + bestTwoSum
+
+            # Update the current positions
+            seqIndex += 1
+            twoSeqIndex += 1
+            threeSeqIndex += 1
+
+        return bestThreeSeq
