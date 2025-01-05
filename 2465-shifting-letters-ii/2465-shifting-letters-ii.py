731df2852334141ba9a6edd69c4d013b34a82e89
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-01-05 20:50:27 +0530

    Time: 61 ms (59.88%), Space: 41.4 MB (30.09%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -1,9 +1,7 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        st="abcdefghijklmnopqrstuvwxyz"
         n = len(s)
         shift_effects = [0] * (n + 1)  
-
         for start, end, direction in shifts:
             if direction == 0:
                 shift_effects[start] -= 1 
@@ -12,17 +10,13 @@ class Solution:
                 shift_effects[start] += 1  
                 shift_effects[end + 1] -= 1
 
-        cumulative_shift = 0
-        shift_amounts = []
-        for shift in shift_effects[:-1]:
-            cumulative_shift += shift
-            shift_amounts.append(cumulative_shift % 26) 
+        for i in range(1,n) :
+            shift_effects[i] = (shift_effects[i-1]+shift_effects[i])%26
         
         ans=list(s)
         for i in range(n) :
-            ind=st.index(s[i])
-            ind=(ind+shift_amounts[i])%26
+            ind=( (ord(s[i])-97) + shift_effects[i])%26
             if ind < 0 :
-                ind =(ind+ 26)%26
-            ans[i]=st[ind]
+                ind =ind+ 26
+            ans[i]=chr(97+ind)
         return "".join(ans)
