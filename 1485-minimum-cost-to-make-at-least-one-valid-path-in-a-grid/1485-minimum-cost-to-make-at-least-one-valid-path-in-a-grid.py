54b379f151f44c3b499caa8872ba5adc96a9cbf4
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-01-18 22:59:51 +0530

    Time: 91 ms (90.58%), Space: 21.8 MB (11.04%) - LeetHub

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minCost(self, A):
+        n, m, inf, k = len(A), len(A[0]), 10**9, 0
+        dp = [[inf] * m for i in range(n)]
+        dirt = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        bfs = []
+
+        def dfs(x, y):
+            if not (0 <= x < n and 0 <= y < m and dp[x][y] == inf): return
+            dp[x][y] = k
+            bfs.append([x, y])
+            dfs(x + dirt[A[x][y] - 1][0], y + dirt[A[x][y] - 1][1])
+
+        dfs(0, 0)
+        while bfs:
+            k += 1
+            bfs, bfs2 = [], bfs
+            [dfs(x + i, y + j) for x, y in bfs2 for i, j in dirt]
+        return dp[-1][-1]
