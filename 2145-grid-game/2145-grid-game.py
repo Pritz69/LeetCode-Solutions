d7502e6ef709655a55ff87d1240c57e6fef73c8a
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-01-21 17:30:51 +0530

    Time: 80 ms (86.56%), Space: 29.6 MB (62.95%) - LeetHub

--- 2145-grid-game/2145-grid-game.py
+++ 2145-grid-game/2145-grid-game.py
@@ -1,15 +1,13 @@
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
-        n=len(grid[0])
-        pr1=grid[0].copy()
-        pr2=grid[1].copy()
-        for i in range(1,n) :
-            pr1[i] += pr1[i-1]
-            pr2[i] += pr2[i-1]
-        res=float('inf')
-        for i in range(n) :
-            t=pr1[-1]-pr1[i]
-            b=pr2[i-1]if i>0 else 0
-            sd=max(t,b)
-            res=min(res,sd)
-        return res
+        n = len(grid[0])
+        ans = math.inf
+        topSum = sum(grid[0])
+        bottomSum = 0
+
+        for i in range(n):
+            topSum -= grid[0][i]
+            ans = min(ans, max(topSum, bottomSum))
+            bottomSum += grid[1][i]
+
+        return ans
