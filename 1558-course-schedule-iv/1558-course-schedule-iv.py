e74c28f1b7b246c7bd69643279b0c0b4fac5f548
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-01-27 13:38:11 +0530

    Time: 623 ms (23.37%), Space: 20.5 MB (34.2%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -0,0 +1,13 @@
+class Solution:
+    def checkIfPrerequisite(self, n: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        connected = [[False] * n for i in range(n)]
+
+        for i, j in prerequisites:
+            connected[i][j] = True
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    connected[i][j] = connected[i][j] or (connected[i][k] and connected[k][j])
+
+        return [connected[i][j] for i, j in queries]
