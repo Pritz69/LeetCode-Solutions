22491394a08a162a7b4ecd6d51f0a355cab81778
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-02-12 13:07:01 +0530

    Time: 267 ms (89.8%), Space: 33.6 MB (52.32%) - LeetHub

---
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        d=defaultdict(list)
+        for x in nums :
+            if x <= 9 :
+                d[x].append(x)
+            else :
+                s=0
+                n=x
+                while n :
+                    di=n%10
+                    s=s+di
+                    n=n//10
+                d[s].append(x)
+        m=-1
+        for x in d :
+            l=sorted(d[x])
+            if len(l) >= 2 :
+                m=max(m,l[-2]+l[-1])
+        return m
