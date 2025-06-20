5a1567d5b25797946a32802d817d3b4215e031f0
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-06-20 13:00:37 +0530

    Time: 1635 ms (89.06%), Space: 18.3 MB (12.5%) - LeetHub

---
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        ans = 0
+        north = south = east = west = 0
+        
+        for i in range(len(s)):
+            c = s[i]
+            if c == 'N':
+                north += 1
+            elif c == 'S':
+                south += 1
+            elif c == 'E':
+                east += 1
+            elif c == 'W':
+                west += 1
+            
+            x = abs(north - south)
+            y = abs(east - west)
+            MD = x + y
+            dis = MD + min(2 * k, i + 1 - MD)
+            ans = max(ans, dis)
+        
+        return ans
