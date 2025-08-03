57253ad9a2e5cd74170e355dedba746a01ca9414
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2025-08-03 18:39:02 +0530

    Time: 678 ms (26.67%), Space: 52.4 MB (80%) - LeetHub

---
+++ 2229-maximum-fruits-harvested-after-at-most-k-steps/2229-maximum-fruits-harvested-after-at-most-k-steps.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxTotalFruits(
+        self, fruits: List[List[int]], startPos: int, k: int
+    ) -> int:
+        n = len(fruits)
+        sum_ = [0] * (n + 1)
+        indices = [0] * n
+
+        for i in range(n):
+            sum_[i + 1] = sum_[i] + fruits[i][1]
+            indices[i] = fruits[i][0]
+
+        ans = 0
+        for x in range(k // 2 + 1):
+            # move left x steps, then right (k - 2x) steps
+            y = k - 2 * x
+            left = startPos - x
+            right = startPos + y
+            start = bisect_left(indices, left)
+            end = bisect_right(indices, right)
+            ans = max(ans, sum_[end] - sum_[start])
+
+            # move right x steps, then left (k - 2x) steps
+            y = k - 2 * x
+            left = startPos - y
+            right = startPos + x
+            start = bisect_left(indices, left)
+            end = bisect_right(indices, right)
+            ans = max(ans, sum_[end] - sum_[start])
+
+        return ans
